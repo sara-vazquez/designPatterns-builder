5c8765e3ac8a101953204aadfe1bdba33a142605
Sara Vázquez <saravazlo@gmail.com>
2025-09-06 12:58:03 +0200

    feat-created interface IBuilder

--- src/main/java/dev/sara/builderPattern/IBuilder.java
+++ src/main/java/dev/sara/builderPattern/IBuilder.java
@@ -1,5 +1,8 @@
 package dev.sara.builderPattern;
 
 public interface IBuilder {
-    
+    IBuilder buildGarage();
+    IBuilder buildPool();
+    IBuilder buildGarden();
+    IBuilder buildFancyStatues();
 }
