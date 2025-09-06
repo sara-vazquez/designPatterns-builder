29995ee58f53105b6fbd014a82f7f37017dcfcb1
Sara Vázquez <saravazlo@gmail.com>
2025-09-06 13:29:10 +0200

    feat-added to main

--- src/main/java/dev/sara/builderPattern/BuilderPatternApplication.java
+++ src/main/java/dev/sara/builderPattern/BuilderPatternApplication.java
@@ -1,13 +1,24 @@
 package dev.sara.builderPattern;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuilderPatternApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuilderPatternApplication.class, args);
+		IBuilder builder = new HouseBuilder();
+		HouseDirector director  = new HouseDirector(builder);
+
+		HouseEntity simpleHouse = director.constructSimpleHouse();
+		HouseEntity summerHouse = director.constructSummerHouse();
+		HouseEntity rusticHouse = director.constructRusticHouse();
+		HouseEntity fancyHouse = director.constructFancyHouse();
+
+		System.out.println(simpleHouse);
+		System.out.println(summerHouse);
+		System.out.println(rusticHouse);
+		System.out.println(fancyHouse);
+
 	}
 
 }
